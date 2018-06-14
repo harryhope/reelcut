1c66d59ea42bed3385ee1c6e0f477ece98c12686
Harry Hope <harry.hope@gmail.com>
2018-06-14 11:46:58 -0400

    add cut + paste methods

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
+const _ = require('lodash')
 const fluentFfmpeg = require('fluent-ffmpeg')
 
-module.exports = (ffmpeg = fluentFfmpeg) => {
+const probe = (ffmpeg, video) => new Promise((resolve, reject) => {
+  ffmpeg.ffprobe(video, (err, metadata) => {
+    if (err) {
+      return reject(err)
+    }
+    return resolve(metadata)
+  })
+})
 
-}
+const createCut = (ffmpeg, video, options) => new Promise((resolve, reject) => {
+  ffmpeg(video)
+    .size(`${options.width}x${options.height}`)
+    .seek(options.seek)
+    .duration(options.length)
+    .noAudio()
+    .save(`${options.outputPath}${options.filename}`)
+    .on('end', () => resolve(`${options.outputPath}${options.filename}`))
+    .on('error', (err) => reject(err))
+})
+
+const merge = (command, path) => new Promise((resolve, reject) => {
+  command
+    .on('end', () => resolve(path))
+    .on('error', (err) => reject(err))
+    .mergeToFile(path)
+})
+
+module.exports = (ffmpeg = fluentFfmpeg) => ({
+  cut: async (video, options = {}) => {
+    options = _.defaults(options, {
+      amount: 4,
+      length: 1.5,
+      width: 660,
+      height: 660,
+      outputPath: './'
+    })
+    const metadata = await probe(ffmpeg, video)
+    const {duration} = metadata.format
+    const cuts = _.range(options.amount).map((index) => {
+      const lowerBound = duration/options.amount * index
+      const upperBound = _.min([_.floor(duration - options.length), duration/options.amount * (index + 1)])
+      const randomSeekTime = _.round(_.random(lowerBound, upperBound), 1)
+      return createCut(ffmpeg, video, Object.assign({}, options, {
+        seek: randomSeekTime,
+        filename: `vid-${index}.mp4`
+      }))
+    })
+    return Promise.all(cuts)
+  },
+
+  paste: async (clips, path) => {
+    const command = ffmpeg()
+    clips.forEach((clip) => command.addInput(clip))
+    return merge(command, path)
+  }
+})
--- test/reelcut-spec.js
+++ test/reelcut-spec.js
@@ -2,9 +2,16 @@ const {expect} = require('chai')
 const reelcut = require('../index')
 
 describe('reelcut', () => {
-  it('should cut a video into hilights', (done) => {
-    reelcut()
-    expect(1).to.equal(1)
-    done()
+  it('should cut a video into hilights', async () => {
+    try {
+      const cuts = await reelcut().cut('./test/sample.mp4', {
+        outputPath: './test/'
+      })
+      expect(cuts).to.be.an('array')
+      const pastes = await reelcut().paste(cuts, './test/final.mp4')
+      expect(pastes).to.be.a('string')
+    } catch (err) {
+      throw err
+    }
   })
 })
